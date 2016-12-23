85e198a3dd1ac3c25060d08cc539241105743adb
Danusia <@DanusiaZabcia>
2016-12-23 13:00:12 +0100

    Add a simple output change

--- kalk/src/kalk/kalkulator.java
+++ kalk/src/kalk/kalkulator.java
@@ -14,6 +14,7 @@ public class kalkulator {
 		System.out.println("a^b= "+ pow(a,b));
 		System.out.println("|a|= "+ abs(a));
 		System.out.println("pierwiastek z a do potegi b = "+ pow(sqrt(abs(a)),b));
+		System.out.println("Test przesy³ania zmiany");
 
 
 	
