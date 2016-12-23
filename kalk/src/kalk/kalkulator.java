de0d416a6a1e0b289fd69b039e691a02e5aed8b4
Danusia <@DanusiaZabcia>
2016-12-23 12:38:02 +0100

    initial checkin

---
+++ kalk/src/kalk/kalkulator.java
@@ -0,0 +1,22 @@
+package kalk;
+import static java.lang.Math.*;
+
+
+public class kalkulator {
+
+	public static void main(String[] args) {
+		
+		int a= -15;
+		int b= 4;
+		
+		
+		
+		System.out.println("a^b= "+ pow(a,b));
+		System.out.println("|a|= "+ abs(a));
+		System.out.println("pierwiastek z a do potegi b = "+ pow(sqrt(abs(a)),b));
+
+
+	
+
+}
+}
