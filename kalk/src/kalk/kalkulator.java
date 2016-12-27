f30d6c06ed1a07025c80bafdd7d42bede3ae7caf
Danusia <Danusia@Zabcia>
2016-12-27 11:57:04 +0100

    dodany komentarz z ćwiczenia

--- kalk/src/kalk/kalkulator.java
+++ kalk/src/kalk/kalkulator.java
@@ -1,6 +1,14 @@
 package kalk;
 import static java.lang.Math.*;
 
+/*Napisz prosty kalkulator, w którym zainicjujesz dwie zmienne int a i b dowolnymi liczbami mieszcz¹cymi siê w zakresie - niech a bêdzie liczb¹ ujemn¹, natomiast b dodatni¹... Nastêpnie wykorzystuj¹c import statyczny wyœwietl wyniki nastêpuj¹cych dzia³añ bez u¿ycia dodatkowych zmiennych:
+-a^b (a do potêgi b)
+
+-|a| (wartoœæ bezwzglêdna z liczby a)
+
+-pierwiastek z liczby a podniesionej do potêgi b.
+
+Zobacz co siê stanie, gdy do a i b przypiszesz dwie 3 cyfrowe liczby, oraz gdy liczba b bêdzie nieparzysta (wynik a^b bêdzie ujemny).  */
 
 public class kalkulator {
 
@@ -9,12 +17,10 @@ public class kalkulator {
 		int a= -15;
 		int b= 4;
 		
-		
-		
 		System.out.println("a^b= "+ pow(a,b));
 		System.out.println("|a|= "+ abs(a));
-		System.out.println("pierwiastek z a do potegi b = "+ pow(sqrt(abs(a)),b));
-		System.out.println("Test przesy³ania zmiany");
+		System.out.println("pierwiastek z a do potegi b = "+ sqrt(pow(a,b)));
+		
 
 
 	
